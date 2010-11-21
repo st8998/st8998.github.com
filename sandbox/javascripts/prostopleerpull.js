2397779fca803b3724f6c9f1de1bf2b8d8e88394
Ivan Efremov <loki@home00.(none)>
2010-11-21 10:07:03 +0300

    player draft

--- sandbox/javascripts/prostopleerpull.js
+++ sandbox/javascripts/prostopleerpull.js
@@ -26,7 +26,8 @@ function ProstopleerPull() {
             this.request.abort();
         }
         this.requestInProgress = true;
-        this.request = $.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fprostopleer.com%2Fsearch%3Fq%3D" + this.currentSearchTerm + "%26page%3D" + this.currentPage + "%22%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22results%22%5D'",
+        var searchTerm = escape(escape(this.currentSearchTerm));
+        this.request = $.get("http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fprostopleer.com%2Fsearch%3Fq%3D" + searchTerm + "%26page%3D" + this.currentPage + "%22%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22results%22%5D'",
                 $.proxy(function(data) {
                     $(data).find('div.track-wrapper').each(function() {
                         var $in = $(this);
@@ -34,7 +35,7 @@ function ProstopleerPull() {
                             artist: $in.find('.artist').text(),
                             title: $in.find('.title').text(),
                             time: $in.find('.track-time p').text(),
-                            url: 'http://prostopleer.com' + $in.find('div.icon > a').attr('href')
+                            url: 'http://prostopleer.com/download/8eve3egzj'// + $in.find('div.icon > a').attr('href')
                         });
                     });
                     this.requestInProgress = false;
